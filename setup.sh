8b53c563b5089ee25c7d05c1e9e9491da74c6d5a
El Yakub <yakubu.umar@aun.edu.ng>
2023-11-16 21:33:20 +0100

    update open interpreter

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Display a welcome message
+# Display a welcome jinnmessage
 echo "Starting the Termux setup..."
 
 # Update and upgrade the package list
@@ -13,7 +13,7 @@ pkg install -y python rust cmake binutils termux-api
 
 # Install Python packages via pip
 echo "Installing Python packages..."
-pip install ruamel.yaml==0.16.12 jinja2 open-interpreter==0.1.8
+pip install ruamel.yaml==0.16.12 jinja2 open-interpreter==0.1.15
 
 # Display a completion message
 echo "Setup completed successfully. You can now use Open Interpreter."
